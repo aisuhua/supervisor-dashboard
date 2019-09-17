37ff142d9b0ca98baa8972a6aa5f997036b32b35
aisuhua <1079087531@qq.com>
2019-09-17 11:12:38 +0800

    add sql and conf

--- schemas/README.md
+++ schemas/README.md
@@ -1,3 +1,12 @@
+
+## Export
+
+```bash
+mysqldump -h localhost -u root -p --no-data supervisor  > supervisor.sql
+```
+
+## Import
+
 ```bash
 mysqladmin -u username -ppassword create supervisor
 mysql -u username -ppassword supervisor < supervisor.sql  
--- schemas/supervisor.sql
+++ schemas/supervisor.sql
@@ -16,13 +16,80 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `program`
+-- Table structure for table `command`
 --
 
-DROP TABLE IF EXISTS `program`;
+DROP TABLE IF EXISTS `command`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `program` (
+CREATE TABLE `command` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `server_id` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '服务器ID',
+  `user` varchar(64) NOT NULL DEFAULT '' COMMENT '执行命令的用户',
+  `command` varchar(255) NOT NULL DEFAULT '' COMMENT '命令',
+  `status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '状态',
+  `start_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '启动时间',
+  `end_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '结束时间',
+  `log` longtext DEFAULT NULL COMMENT '日志内容',
+  `update_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '修改时间',
+  `create_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '添加时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=utf8mb4 COMMENT='命令执行记录表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cron`
+--
+
+DROP TABLE IF EXISTS `cron`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cron` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `server_id` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '服务器ID',
+  `user` varchar(64) NOT NULL DEFAULT '' COMMENT '执行命令的用户',
+  `command` varchar(255) NOT NULL DEFAULT '' COMMENT '命令',
+  `time` varchar(64) NOT NULL DEFAULT '' COMMENT '运行时间',
+  `status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '状态',
+  `description` varchar(255) NOT NULL DEFAULT '' COMMENT '说明',
+  `last_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '上次运行时间',
+  `update_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '修改时间',
+  `create_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '添加时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COMMENT='定时任务表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cron_log`
+--
+
+DROP TABLE IF EXISTS `cron_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cron_log` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `cron_id` int(11) unsigned NOT NULL DEFAULT 0 COMMENT '定时任务ID',
+  `server_id` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '服务器ID',
+  `program` varchar(255) NOT NULL DEFAULT '' COMMENT '定时任务名称',
+  `command` varchar(255) NOT NULL DEFAULT '' COMMENT '命令',
+  `status` smallint(4) NOT NULL DEFAULT 0 COMMENT '执行状态',
+  `start_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '启动时间',
+  `end_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '结束时间',
+  `log` longtext DEFAULT NULL COMMENT '日志内容',
+  `update_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '修改时间',
+  `create_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '添加时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40159 DEFAULT CHARSET=utf8mb4 COMMENT='定时任务执行记录表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `process`
+--
+
+DROP TABLE IF EXISTS `process`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `process` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `server_id` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '服务器ID',
   `program` varchar(64) NOT NULL DEFAULT '' COMMENT '程序名',
@@ -45,18 +112,8 @@ CREATE TABLE `program` (
   KEY `program` (`program`),
   KEY `server_id` (`server_id`),
   KEY `create_time` (`create_time`)
-) ENGINE=InnoDB AUTO_INCREMENT=1370 DEFAULT CHARSET=utf8mb4 COMMENT='进程配置表';
+) ENGINE=InnoDB AUTO_INCREMENT=4208 DEFAULT CHARSET=utf8mb4 COMMENT='进程配置表';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `program`
---
-
-LOCK TABLES `program` WRITE;
-/*!40000 ALTER TABLE `program` DISABLE KEYS */;
-INSERT INTO `program` VALUES (357,47,'SUPERVISOR_COMMAND_EXEC_SERVICE','/usr/bin/php /www/web/supervisor.ops.115.com/worker/SUPERVISOR_COMMAND_EXEC_SERVICE.php --server-ip=172.16.210.76','%(program_name)s_%(process_num)s',1,0,'%(here)s','true',20,'true','','true','AUTO',0,'1M',1566983140,1566983140),(358,47,'cat1','/bin/cat','%(program_name)s_%(process_num)s',1,0,'%(here)s','true',20,'false','','false','AUTO',1,'10M',1566983171,1566983171),(1129,46,'cat2','/bin/cat','%(program_name)s_%(process_num)s',1,0,'%(here)s','true',20,'false','www-data','true','AUTO',0,'1M',0,0),(1130,46,'FOLDER_ID_TRANSCODE','/usr/bin/php /www/web/site.115.com/worker/transcode/FOLDER_ID_TRANSCODE.php','%(program_name)s_%(process_num)s',1,0,'%(here)s','true',20,'true','www-data','true','AUTO',0,'1M',0,0),(1131,46,'PUSHARE_CLEAN_EXPIRED_NEW','/usr/bin/php /www/web/site.115.com/worker/pushare/PUSHARE_CLEAN_EXPIRED_NEW.php','%(program_name)s_%(process_num)s',1,0,'%(here)s','true',20,'true','www-data','true','AUTO',0,'1M',0,0),(1132,46,'PUSHARE_RECV_FILE_0','/usr/bin/php /www/web/site.115.com/worker/pushare/PUSHARE_RECV_FILE.php -u  %(process_num)s','%(program_name)s_%(process_num)s',1,0,'%(here)s','true',20,'true','www-data','true','AUTO',0,'1M',0,0),(1366,41,'aria2c','aria2c --conf-path=/etc/aria2c.conf','%(program_name)s_%(process_num)s',1,0,'%(here)s','true',20,'true','suhua','true','AUTO',0,'1MB',1567419768,0),(1367,41,'cat1','/bin/cat','%(program_name)s_%(process_num)s',1,0,'%(here)s','true',20,'true','www-data','true','AUTO',0,'1MB',1567471865,0),(1368,41,'demo','php /www/web/demo.php','%(program_name)s_%(process_num)s',3,0,'%(here)s','true',20,'true','www-data','true','AUTO',0,'1MB',1567472097,0);
-/*!40000 ALTER TABLE `program` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `server`
@@ -72,27 +129,19 @@ CREATE TABLE `server` (
   `port` int(10) unsigned NOT NULL COMMENT 'XML-RPC 端口',
   `username` varchar(30) NOT NULL DEFAULT '' COMMENT 'XML-RPC 用户名',
   `password` varchar(255) NOT NULL DEFAULT '' COMMENT 'XML-RPC 密码',
-  `conf_path` varchar(255) NOT NULL DEFAULT '' COMMENT '配置文件写入的路径',
+  `process_conf` varchar(255) NOT NULL DEFAULT '' COMMENT '进程配置',
   `sync_conf_port` smallint(11) unsigned NOT NULL DEFAULT 0 COMMENT '更新配置服务所监听的端口',
   `sort` smallint(11) unsigned NOT NULL DEFAULT 0 COMMENT '排序字段',
   `update_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '修改时间',
   `create_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '添加时间',
+  `cron_conf` varchar(255) NOT NULL DEFAULT '' COMMENT '定时任务配置',
+  `command_conf` varchar(255) NOT NULL DEFAULT '' COMMENT '命令配置',
   PRIMARY KEY (`id`),
   UNIQUE KEY `ip_port` (`ip`,`port`),
   KEY `group_id` (`server_group_id`),
   KEY `sort` (`sort`)
-) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8mb4 COMMENT='服务器表';
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COMMENT='服务器表';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `server`
---
-
-LOCK TABLES `server` WRITE;
-/*!40000 ALTER TABLE `server` DISABLE KEYS */;
-INSERT INTO `server` VALUES (41,161,'192.168.1.229',9001,'worker','111111','/etc/supervisor/conf.d/program.conf',8089,0,1567147230,1566459420),(44,99,'172.16.0.63',9001,'worker','111111','/etc/supervisor/conf.d/program.conf',8089,0,1567147277,1566464463),(46,99,'172.16.0.61',9001,'worker','111111','/etc/supervisor/conf.d/program.conf',8089,11,1567147488,1566475933),(47,99,'172.16.0.69',9001,'worker','111111','/etc/supervisor/conf.d/program.conf',8089,0,1567147363,1566475973);
-/*!40000 ALTER TABLE `server` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `server_group`
@@ -111,22 +160,8 @@ CREATE TABLE `server_group` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
   KEY `sort` (`sort`)
-) ENGINE=InnoDB AUTO_INCREMENT=193 DEFAULT CHARSET=utf8mb4 COMMENT='服务器分组表';
+) ENGINE=InnoDB AUTO_INCREMENT=194 DEFAULT CHARSET=utf8mb4 COMMENT='服务器分组表';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `server_group`
---
-
-LOCK TABLES `server_group` WRITE;
-/*!40000 ALTER TABLE `server_group` DISABLE KEYS */;
-INSERT INTO `server_group` VALUES (99,'115内网','115内网',10,1566483736,0),(161,'默认分组','系统内置分组',100,1566822895,1566348880);
-/*!40000 ALTER TABLE `server_group` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'supervisor'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -137,4 +172,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-03 10:53:55
+-- Dump completed on 2019-09-17 11:11:55
